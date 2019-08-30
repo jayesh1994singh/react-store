3cd2ba894f0b44c74a84b29e03a1993dbd1a298e
jayesh1994singh <jayesh1994singh@gmail.com>
2019-08-30 14:43:27 +0530

    first commit

---
+++ src/Data.js
@@ -0,0 +1,171 @@
+export const storeProducts = [
+    {
+        id: 1,
+        title: "indoor plants",
+        img: "img/1.jpg",
+
+        price: 23,
+        info:
+            "A houseplant is a plant that is grown indoors in places such as residences and offices, namely for decorative purposes, but studies have also shown them to have positive psychological effects and as well as help with indoor air purification, since some species, and the soil-dwelling microbes associated with them, reduce indoor air pollution by absorbing volatile organic compounds including benzene, formaldehyde, and trichloroethylene",
+
+        inCart: false,
+        count: 0,
+        total: 0
+    },
+    {
+        id: 2,
+        title: "indoor",
+        img: " img/2.jpg",
+
+        price: 26,
+        info: "A houseplant is a plant that is grown indoors in places such as residences and offices, namely for decorative purposes, but studies have also shown them to have positive psychological effects and as well as help with indoor air purification, since some species, and the soil-dwelling microbes associated with them, reduce indoor air pollution by absorbing volatile organic compounds including benzene, formaldehyde, and trichloroethylene",
+        inCart: false,
+        count: 0,
+        total: 0
+    },
+
+    {
+        id: 3,
+        title: "indoor plants",
+        img: " img/3.jpg",
+
+        price: 36,
+        info: "A houseplant is a plant that is grown indoors in places such as residences and offices, namely for decorative purposes, but studies have also shown them to have positive psychological effects and as well as help with indoor air purification, since some species, and the soil-dwelling microbes associated with them, reduce indoor air pollution by absorbing volatile organic compounds including benzene, formaldehyde, and trichloroethylene",
+        inCart: false,
+        count: 0,
+        total: 0
+    },
+
+    {
+        id: 4,
+        title: "indoor plants",
+        img: " img/4.jpg",
+
+        price: 46,
+        info: "A houseplant is a plant that is grown indoors in places such as residences and offices, namely for decorative purposes, but studies have also shown them to have positive psychological effects and as well as help with indoor air purification, since some species, and the soil-dwelling microbes associated with them, reduce indoor air pollution by absorbing volatile organic compounds including benzene, formaldehyde, and trichloroethylene",
+        inCart: false,
+        count: 0,
+        total: 0
+    },
+
+    {
+        id: 5,
+        title: "indoor plants",
+        img: "img/5.jpg ",
+
+        price: 52,
+        info: "A houseplant is a plant that is grown indoors in places such as residences and offices, namely for decorative purposes, but studies have also shown them to have positive psychological effects and as well as help with indoor air purification, since some species, and the soil-dwelling microbes associated with them, reduce indoor air pollution by absorbing volatile organic compounds including benzene, formaldehyde, and trichloroethylene",
+        inCart: false,
+        count: 0,
+        total: 0
+    },
+
+    {
+        id: 6,
+        title: "indoor plants",
+        img: " img/6.jpg",
+
+        price: 56,
+        info: "A houseplant is a plant that is grown indoors in places such as residences and offices, namely for decorative purposes, but studies have also shown them to have positive psychological effects and as well as help with indoor air purification, since some species, and the soil-dwelling microbes associated with them, reduce indoor air pollution by absorbing volatile organic compounds including benzene, formaldehyde, and trichloroethylene",
+        inCart: false,
+        count: 0,
+        total: 0
+    },
+
+    {
+        id: 7,
+        title: "indoor plants",
+        img: "img/7.jpg ",
+
+        price: 21,
+        info: "A houseplant is a plant that is grown indoors in places such as residences and offices, namely for decorative purposes, but studies have also shown them to have positive psychological effects and as well as help with indoor air purification, since some species, and the soil-dwelling microbes associated with them, reduce indoor air pollution by absorbing volatile organic compounds including benzene, formaldehyde, and trichloroethylene",
+        inCart: false,
+        count: 0,
+        total: 0
+    },
+
+    {
+        id: 8,
+        title: "indoor plants",
+        img: "img/8.jpg ",
+
+        price: 15,
+        info: "A houseplant is a plant that is grown indoors in places such as residences and offices, namely for decorative purposes, but studies have also shown them to have positive psychological effects and as well as help with indoor air purification, since some species, and the soil-dwelling microbes associated with them, reduce indoor air pollution by absorbing volatile organic compounds including benzene, formaldehyde, and trichloroethylene",
+        inCart: false,
+        count: 0,
+        total: 0
+    },
+
+    {
+        id: 9,
+        title: "indoor plants",
+        img: "img/9.jpg ",
+
+        price: 12,
+        info: "A houseplant is a plant that is grown indoors in places such as residences and offices, namely for decorative purposes, but studies have also shown them to have positive psychological effects and as well as help with indoor air purification, since some species, and the soil-dwelling microbes associated with them, reduce indoor air pollution by absorbing volatile organic compounds including benzene, formaldehyde, and trichloroethylene",
+        inCart: false,
+        count: 0,
+        total: 0
+    },
+
+    {
+        id: 10,
+        title: "indoor plants",
+        img: " img/10.jpg",
+
+        price: 5,
+        info: "A houseplant is a plant that is grown indoors in places such as residences and offices, namely for decorative purposes, but studies have also shown them to have positive psychological effects and as well as help with indoor air purification, since some species, and the soil-dwelling microbes associated with them, reduce indoor air pollution by absorbing volatile organic compounds including benzene, formaldehyde, and trichloroethylene",
+        inCart: false,
+        count: 0,
+        total: 0
+    },
+    {
+        id: 11,
+        title: "indoor plants",
+        img: "img/11.jpg ",
+
+        price: 8,
+        info: "A houseplant is a plant that is grown indoors in places such as residences and offices, namely for decorative purposes, but studies have also shown them to have positive psychological effects and as well as help with indoor air purification, since some species, and the soil-dwelling microbes associated with them, reduce indoor air pollution by absorbing volatile organic compounds including benzene, formaldehyde, and trichloroethylene",
+        inCart: false,
+        count: 0,
+        total: 0
+    },
+
+    {
+        id: 12,
+        title: "indoor plants",
+        img: " img/12.jpg",
+
+        price: 16,
+        info: "A houseplant is a plant that is grown indoors in places such as residences and offices, namely for decorative purposes, but studies have also shown them to have positive psychological effects and as well as help with indoor air purification, since some species, and the soil-dwelling microbes associated with them, reduce indoor air pollution by absorbing volatile organic compounds including benzene, formaldehyde, and trichloroethylene",
+        inCart: false,
+        count: 0,
+        total: 0
+    },
+
+    {
+        id: 13,
+        title: "indoor plants",
+        img: "img/13.jpg ",
+
+        price: 20,
+        info: "A houseplant is a plant that is grown indoors in places such as residences and offices, namely for decorative purposes, but studies have also shown them to have positive psychological effects and as well as help with indoor air purification, since some species, and the soil-dwelling microbes associated with them, reduce indoor air pollution by absorbing volatile organic compounds including benzene, formaldehyde, and trichloroethylene",
+        inCart: false,
+        count: 0,
+        total: 0
+    }
+];
+export const detailProducts = {
+
+    id: 1,
+    title: "indoor plants",
+    img: "img/1.jpg",
+
+    price: 23,
+    info:
+        "A houseplant is a plant that is grown indoors in places such as residences and offices, namely for decorative purposes, but studies have also shown them to have positive psychological effects and as well as help with indoor air purification, since some species, and the soil-dwelling microbes associated with them, reduce indoor air pollution by absorbing volatile organic compounds including benzene, formaldehyde, and trichloroethylene",
+
+    inCart: false,
+    count: 0,
+    total: 0
+};
